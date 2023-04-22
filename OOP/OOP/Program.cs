0f8a7a309463d3c70291b5d81c1a684d728bf9bb
olezh <olezhka1629@gmail.com>
2023-04-22 18:27:16 +0300

    add reference to Microsoft.Speech.dll but smt wrong with language

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,8 +1,11 @@
 ﻿using OOP;
+using OOP.VoiceService;
 
 string email = "okezdfs@mail.ru";
 string login = "ewrwer";
 string password = "12312.../__Olreg";
+
+SpeachRecognition recognition = new SpeachRecognition();
 
 User user = new User(email,login,password);
 Console.WriteLine(user.EmailValidation(user.Email));
@@ -18,22 +21,36 @@ Console.WriteLine("You can chouse comand from next title, please enter index of 
 int a = 0;
 Console.WriteLine("Instruction");
 Console.WriteLine();
+Console.WriteLine("Enter 0, if you want ended app");
 Console.WriteLine("Enter 1, if you want enter your email");
 Console.WriteLine("Enter 2, if you want enter your password");
 Console.WriteLine("Enter 3, if you want add question");
+Console.WriteLine("Enter 4, if you want recognize your speech");
 
-switch(a)
-{
-    case 1:
-        string? email1  = Console.ReadLine();
-        Console.WriteLine(user.EmailValidation(email1));
-        break;
-    case 2:
-        string? password1 = Console.ReadLine();
-        Console.WriteLine(user.PasswordValidation(password1));
-        break;
 
-    case 3:
 
-        break;
+while(true) {
+    a = Int32.Parse(Console.ReadLine());
+    switch (a)
+    {
+        case 0:
+            break;
+        case 1:
+            string? email1 = Console.ReadLine();
+            Console.WriteLine(user.EmailValidation(email1));
+            break;
+        case 2:
+            string? password1 = Console.ReadLine();
+            Console.WriteLine(user.PasswordValidation(password1));
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+            recognition.Recognize();
+            break;
+    }
+
 }
---
+++ OOP/OOP/VoiceService/SpeachRecognition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CognitiveServices.Speech;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Speech.Recognition;
+
+
+
+namespace OOP.VoiceService
+{
+    internal class SpeachRecognition
+    {
+        public void Recognize()
+        {
+            using (
+                  SpeechRecognitionEngine recognizer =
+                    new SpeechRecognitionEngine(
+                      new System.Globalization.CultureInfo("en-US")))
+            {
+                // Create and load a dictation grammar.  
+               // recognizer.LoadGrammar(new Microsoft.Speech.Recognition.DictationGrammar());
+
+                // Add a handler for the speech recognized event.  
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                // Configure input to the speech recognizer.  
+                recognizer.SetInputToDefaultAudioDevice();
+
+                // Start asynchronous, continuous speech recognition.  
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                recognizer.SetInputToDefaultAudioDevice();
+
+                // Start asynchronous, continuous speech recognition.  
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open.  
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+
+            }
+        }
+
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Recognized text: " + e.Result.Text);
+        }
+    } 
+}
