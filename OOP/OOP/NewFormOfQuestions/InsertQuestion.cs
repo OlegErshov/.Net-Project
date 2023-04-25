6c05ecb5ad824c541c050f0e4ee5482e3624e38f
olezh <olezhka1629@gmail.com>
2023-04-25 14:58:43 +0300

    fix previous issue and add sentence question and task

--- OOP/OOP/NewFormOfQuestions/InsertQuestion.cs
+++ OOP/OOP/NewFormOfQuestions/InsertQuestion.cs
@@ -8,5 +8,14 @@ namespace OOP.NewFormOfQuestions
 {
     internal class InsertQuestion
     {
+        // вопрос предназначенный для вставки подходящего по смыслу из списка, который хрнаится в задании такого типа
+        public  string? word { get; set; }
+        public string? sentence { get; set; }
+
+        public InsertQuestion(string? word, string? sentence)
+        {
+            this.word = word;
+            this.sentence = sentence;
+        }
     }
 }
--- OOP/OOP/Tasks/GrammaTask.cs
+++ OOP/OOP/Tasks/GrammaTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP.NewFormOfQuestions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace OOP.Tasks
 {
     internal class GrammaTask
     {
+        List<GrammaQuestion> questions { get; set; }    
+
+        void  AddQuestion(string sent,List<string> varinets, List<string> answer)
+        {
+            GrammaQuestion q = new GrammaQuestion(sent, varinets, answer);
+            questions.Add(q);
+        }
+
+        void AddQuestion(GrammaQuestion question)
+        {
+            questions.Add(question);
+        }
+
+       // проверить сразу все монжо храня лист листов ответов пользователя, но зачем, если можно по одному проверять
     }
 }
