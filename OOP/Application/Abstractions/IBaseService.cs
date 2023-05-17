7c25fcd35436f7f74661c92e868f900255297a23
olezh <olezhka1629@gmail.com>
2023-05-17 21:02:50 +0300

    Добавил прослойку между визуалом и бд и собсвтенно сервисы для работы с бд

---
+++ OOP/Application/Abstractions/IBaseService.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public  interface IBaseService<T> where T : User
+    {
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default,
+            params Expression<Func<T, object>>[]? includesProperties);
+
+        Task AddAsync(T item, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(T item, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(T item, CancellationToken cancellationToken = default);
+
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter, CancellationToken
+       cancellationToken = default);
+
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> filter,
+            CancellationToken cancellationToken = default,
+            params Expression<Func<T, object>>[]? includesProperties);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ OOP/Application/Services/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using Repository.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class StudentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PositionService(IUnitOfWork unit)
+        {
+            _unitOfWork = unit;
+        }
+    }
+}
