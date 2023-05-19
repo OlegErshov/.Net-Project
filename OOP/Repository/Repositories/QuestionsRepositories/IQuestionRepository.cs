c7adae5e23a71e7d5c77bcc8c046ca27b30b7697
olezh <olezhka1629@gmail.com>
2023-05-19 22:41:10 +0300

    add question repository

---
+++ OOP/Repository/Repositories/QuestionsRepositories/IQuestionRepository.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories.QuestionsRepositories
+{
+    public interface IQuestionRepository<T> where T : AnswerTemplate
+    {
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default,
+                                params Expression<Func<T, object>>[]? includesProperties);
+
+        Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> filter,
+                                        CancellationToken cancellationToken = default,
+                                        params Expression<Func<T, object>>[]? includesProperties);
+
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter, CancellationToken
+       cancellationToken = default);
+    }
+}
