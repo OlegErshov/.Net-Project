a97b43b80867b5d81cf4b3cae22b0ccbd2f8b56e
olezh <olezhka1629@gmail.com>
2023-05-22 15:40:28 +0300

    save beforre update database

--- OOP/Plugin/Data/User.cs
+++ OOP/Plugin/Data/User.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Plugin.Authorization
 {
-    public class User
+    public class User : IdentityUser<string>
     {
         
 
